4a4dff2cdc37e24d4980baf0efc2ddf16eba4269
ttiurt <truittzach33@gmail.com>
2023-05-30 17:54:01 -0400

    add font + hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './App.css'
+
+import Hero from './components/Hero/Hero'
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Hero />
     </>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import styles from './Hero.module.css'
+
+const Hero = () => {
+  return (
+    <main className={styles.container}>
+      <h1>zach TRUITT</h1>
+    </main>
+  )
+}
+
+export default Hero
