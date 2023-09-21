b17928e7eb34b9e4d10c5ee93ece798d69edb312
ttiurt <truittzach33@gmail.com>
2023-09-20 22:46:03 -0400

    add snap scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 //import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home/Home'
-import Hero from './components/Hero/Hero'
 
 function App() {
   return (
     <>
-      <Hero />
       <Home />
     </>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
-//import MyProjects from '../../components/MyProjects/MyProject'
+import Hero from '../../components/Hero/Hero'
+import MyProjects from '../../components/MyProjects/MyProject'
 
 import styles from './Home.module.css'
 const Home = () => {
   return( 
     <>
       <div className={styles.container}>
-        
+        <Hero />
+        <MyProjects />
       </div>
     </>
   )
