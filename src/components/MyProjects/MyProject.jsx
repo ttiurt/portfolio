c2255257a7ab224e7b971948c6183994f9c5faa0
ttiurt <truittzach33@gmail.com>
2023-09-26 13:34:45 -0400

    add grid for projects

--- src/components/MyProjects/MyProject.jsx
+++ src/components/MyProjects/MyProject.jsx
@@ -7,48 +7,43 @@ const MyProjects = () => {
       <div className={styles.thngsTitle}>
         <h1>THINGS I'VE MADE</h1>
       </div>
-      <div className={styles.thngLeft}>
-        <div className={styles.TLinfo}>
-          <h1>Lap Logger</h1>
-          <br />
-          <p>TypeScript, JavaScript, React, CSS, MUI</p>
-          <br />
-          <a href="https://thelaplogger.com/" target="_blank" rel="noopener noreferrer">Deployment</a>
-          <br />
-          <a href="https://github.com/ttiurt/laplogger-face" target="_blank" rel="noopener noreferrer">Github</a>
+      <br />
+      <div className={styles.projects}>
+        <div className={styles.thng1}>
+          <div className={styles.thngInfo}>
+            <h1>Lap Logger</h1>
+            <p>TypeScript, JavaScript, React, CSS, MUI</p>
+            <a href="https://thelaplogger.com/" target="_blank" rel="noopener noreferrer">Deployment</a>
+            <br />
+            <a href="https://github.com/ttiurt/laplogger-face" target="_blank" rel="noopener noreferrer">Github</a>
+          </div>
         </div>
-      </div>
-      <div className={styles.thngLeft}>
-        <div className={styles.TLinfo}>
-          <h1>BarkBase</h1>
-          <br />
-          <p>JavaScript, React, CSS, MongoDB, MUI</p>
-          <br />
-          <a href="https://bark-base.netlify.app/" target="_blank" rel="noopener noreferrer">Deployment</a>
-          <br />
-          <a href="https://github.com/AndrewRentschler/barkbase-front-end" target="_blank" rel="noopener noreferrer">Github</a>
+        <div className={styles.thng2}>
+          <div className={styles.thngInfo}>
+            <h1>BarkBase</h1>
+            <p>JavaScript, React, CSS, MongoDB, MUI</p>
+            <a href="https://bark-base.netlify.app/" target="_blank" rel="noopener noreferrer">Deployment</a>
+            <br />
+            <a href="https://github.com/AndrewRentschler/barkbase-front-end" target="_blank" rel="noopener noreferrer">Github</a>
+          </div>
         </div>
-      </div>
-      <div className={styles.thngLeft}>
-        <div className={styles.TLinfo}>
-          <h1>C.D.L.F.G.</h1>
-          <br />
-          <p>JavaScript, CSS, MongoDB, Google OAuth</p>
-          <br />
-          <a href="https://cdlfg.fly.dev/home" target="_blank" rel="noopener noreferrer">Deployment</a>
-          <br />
-          <a href="https://github.com/ttiurt/cdlfg-forum" target="_blank" rel="noopener noreferrer">Github</a>
+        <div className={styles.thng3}>
+          <div className={styles.thngInfo}>
+            <h1>C.D.L.F.G.</h1>
+            <p>JavaScript, CSS, MongoDB, Google OAuth</p>
+            <a href="https://cdlfg.fly.dev/home" target="_blank" rel="noopener noreferrer">Deployment</a>
+            <br />
+            <a href="https://github.com/ttiurt/cdlfg-forum" target="_blank" rel="noopener noreferrer">Github</a>
+          </div>
         </div>
-      </div>
-      <div className={styles.thngLeft}>
-        <div className={styles.TLinfo}>
-          <h1>Truitt's Sports Trivia</h1>
-          <br />
-          <p>JavaScript, HTML, CSS</p>
-          <br />
-          <a href="https://truitts-trivia.netlify.app/" target="_blank" rel="noopener noreferrer">Deployment</a>
-          <br />
-          <a href="https://github.com/ttiurt/truitts-sports-trivia" target="_blank" rel="noopener noreferrer">Github</a>
+        <div className={styles.thng4}>
+          <div className={styles.thngInfo}>
+            <h1>Truitt's Sports Trivia</h1>
+            <p>JavaScript, HTML, CSS</p>
+            <a href="https://truitts-trivia.netlify.app/" target="_blank" rel="noopener noreferrer">Deployment</a>
+            <br />
+            <a href="https://github.com/ttiurt/truitts-sports-trivia" target="_blank" rel="noopener noreferrer">Github</a>
+          </div>
         </div>
       </div>
     </div>
